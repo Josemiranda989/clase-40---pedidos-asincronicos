dae0db80f02db4cdae14f8208bdddd259dd2c94e
José Miranda <josemiranda989@gmail.com>
2022-12-08 22:09:45 -0300

    update with comments

---
+++ frontend/js/favoritas.js
@@ -0,0 +1,56 @@
+window.onload = () => {
+  const app = document.getElementById("root");
+  const container = document.createElement("div");
+  container.setAttribute("class", "container");
+  app.appendChild(container);
+
+  // Obtenemos los datos de localStorage
+  let str = localStorage.getItem("datos");
+  // convertimos ese string en un array
+  let arr = str.split(",");
+  // Creamos una nueva instancia de arr y hacemos uso de Set para no colocar los elementos repetidos https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+  let favoritas = Array.from(new Set(arr));
+
+  // Realizamos un pedido a nuestra api de todas las peliculas
+  fetch(`http://localhost:3031/api/movies/`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (peli) {
+      let movies = peli.data;
+
+      // filtramos las peliculas que coincidan con los id obtenidos de localstorage y lo guardamos en un nuevo array
+      const filterMovies = movies.filter((movie) => {
+        return favoritas.includes(String(movie.id));
+      });
+
+      console.log(filterMovies);
+      // iteamos ese nuevo array con su respectiva tarjeta
+      filterMovies.forEach((movie) => {
+        const card = document.createElement("div");
+        card.setAttribute("class", "card");
+        container.appendChild(card);
+
+        const h1 = document.createElement("h1");
+        h1.textContent = movie.title;
+        card.appendChild(h1);
+
+        const p = document.createElement("p");
+        p.textContent = `Rating: ${movie.rating}`;
+        card.appendChild(p);
+
+        const duracion = document.createElement("p");
+        duracion.textContent = `Duración: ${movie.length}`;
+        card.appendChild(duracion);
+
+        if (movie.genre !== null) {
+          const genero = document.createElement("p");
+          genero.textContent = `Genero: ${movie.genre.name}`;
+          card.appendChild(genero);
+        }
+      });
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+};
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,17 +1,19 @@
-function miCalificacion(id) {
-  
-  let datos = localStorage.getItem('datos');
-  if(datos !== null) {
-    let arr = datos.split(',');
+// Función que recibe el click de la estrella
+function agregarFavorito(id) {
+  //  Obtenemos los favoritos de localStorage (strings)
+  let datos = localStorage.getItem("datos");
+  if (datos !== null) {
+    // Si contiene datos con split convertimos ese string en array para agregar la nueva película
+    let arr = datos.split(",");
     arr.push(id);
-    localStorage.setItem('datos', arr);
+    localStorage.setItem("datos", arr);
     console.log(localStorage.getItem("datos"));
   } else {
-    localStorage.setItem('datos', id);
-    console.log(localStorage.getItem('datos'));
-  }  
+    // Si no contiene nada en localStorage seteamos la nueva película
+    localStorage.setItem("datos", id);
+    console.log(localStorage.getItem("datos"));
+  }
 }
-
 
 window.onload = () => {
   const app = document.getElementById("root");
@@ -19,51 +21,52 @@ window.onload = () => {
   container.setAttribute("class", "container");
   app.appendChild(container);
 
-  if(localStorage.getItem('datos') !== null) {
+  // Si contiene favoritos en localStorage imprime link a favoritas
+  if (localStorage.getItem("datos") !== null) {
     const link = document.getElementById("favoritas");
-    link.innerHTML = '<a href="favoritas.html">Favoritas</a>';
+    link.innerHTML = '<a class="button" href="favoritas.html">Favoritas</a>';
   }
 
+  // Fetch a nuestro endpoint para obtener todas las peliculas
   fetch("http://localhost:3031/api/movies")
     .then(function (response) {
       return response.json();
     })
     .then(function (peliculas) {
       let data = peliculas.data;
-      
+
+      // iteramos por cada pelicula para imprimir tarjetas
       data.forEach((movie) => {
         const card = document.createElement("div");
         card.setAttribute("class", "card");
+        container.appendChild(card);
 
         const h1 = document.createElement("h1");
         h1.textContent = movie.title;
-        
+        card.appendChild(h1);
 
         const p = document.createElement("p");
         p.textContent = `Rating: ${movie.rating}`;
+        card.appendChild(p);
 
         const duracion = document.createElement("p");
         duracion.textContent = `Duración: ${movie.length}`;
+        card.appendChild(duracion);
 
-        container.appendChild(card);
-        card.appendChild(h1);
-       
-        card.appendChild(p);
         if (movie.genre !== null) {
           const genero = document.createElement("p");
           genero.textContent = `Genero: ${movie.genre.name}`;
           card.appendChild(genero);
         }
 
+        // Agregamos la estrella de favoritos con un evento onclick que ejecuta la función agregarFavorito
         const favoritas = document.createElement("p");
         favoritas.innerHTML = `
         <div class="ec-stars-wrapper">
-          <a href="#" onclick="miCalificacion(${movie.id})">&#9733;</a>
+        <a href="#" onclick="agregarFavorito(${movie.id})">&#9733;</a>
         </div>
         `;
-        
-        card.appendChild(duracion)
-        card.appendChild(favoritas)
+        card.appendChild(favoritas);
       });
     })
     .catch(function (error) {
